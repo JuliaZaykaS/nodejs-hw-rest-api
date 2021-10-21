69a9d3259fb48f1e4e16a56ab5f0c179409ae8f9
JuliaZaykaS <sys55@mail.ru>
2021-10-21 10:13:34 +0300

    change env example

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@zaykacluster.t7imb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 const connectMongoDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL, { useNewURLParser: true, useUnifiedTopology: true })
+  await mongoose.connect(DB_URI, { useNewURLParser: true, useUnifiedTopology: true })
 }
 
 module.exports = { connectMongoDB }
