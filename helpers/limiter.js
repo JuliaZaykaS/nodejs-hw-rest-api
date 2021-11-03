4338fcb1453250fec7e8008831f1a4598bfe6b46
JuliaZaykaS <sys55@mail.ru>
2021-11-03 15:59:49 +0300

    step 1 - 3

--- helpers/limiter.js
+++ helpers/limiter.js
@@ -11,7 +11,8 @@ const apiLimiter = rateLimit({
 
 const createAccountLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour window
-  max: 5, // start blocking after 5 requests
+  // max: 5, // start blocking after 5 requests
+  max: 100,
   handler: (req, res, next) => {
     throw new LimiterError('Too many accounts created from this IP, please try again after an hour')
   }
--- model/users/registration.js
+++ model/users/registration.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 require('dotenv').config()
+const gravatar = require('gravatar')
 const { User } = require('../../db')
 const { AuthenticationError } = require('../../helpers')
 
@@ -16,8 +17,9 @@ const registration = async (body) => {
   }
 
   const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT))
+  const avatarURL = gravatar.url(email, { protocol: 'https', d: 'mp' })
 
-  const user = new User({ email, password: hashedPassword })
+  const user = new User({ email, password: hashedPassword, avatarURL })
   await user.save()
   return user
 }
