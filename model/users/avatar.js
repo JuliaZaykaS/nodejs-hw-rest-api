dff06b4d533d4dfaf9787b23152edf240ad8d734
JuliaZaykaS <sys55@mail.ru>
2021-11-06 16:15:23 +0300

    jimp

--- model/users/avatar.js
+++ model/users/avatar.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs').promises
+const Jimp = require('jimp')
 
 const { User } = require('../../db')
 const { AuthorizationError } = require('../../helpers')
@@ -10,9 +11,10 @@ const avatarDir = path.join(__dirname, '../../', '/public/avatars') // storeImag
 const avatar = async (user, file) => {
   const { path: tempPath, originalname } = file
   const { _id } = user
-  console.log(tempPath)
+  console.log('tempPath before', tempPath)
   // const avatarDir = path.join(__dirname, '../../', '/public/avatars', String(_id))
   // console.log(avatarDir)
+  const newTempPath = tempPath
 
   if (!user) {
     throw new AuthorizationError('Not authorized')
@@ -24,25 +26,62 @@ const avatar = async (user, file) => {
   // }
 
   const userDir = path.join(avatarDir, String(_id))
-  console.log('userdir', userDir)
+  // console.log('userdir', userDir)
   try {
+    // let isAccessibleDir = await fs.access(userDir)
     const isAccessibleDir = await fs.access(userDir)
     console.log('isAccessibleDir', isAccessibleDir)
-    if (!isAccessibleDir) {
-      // await fs.unlink(userDir)
-      // await fs.rmdir(userDir)
+    // if (!isAccessibleDir) {
+    // if (isAccessibleDir) {
+    // if (isAccessibleDir === undefined) {
+    //   isAccessibleDir = true
+    //   // } else { isAccessibleDir = false }
+    // }
+    // await fs.unlink(userDir)
+    // await fs.rmdir(userDir)
+    console.log('isAccessibleDir', isAccessibleDir)
+    // if (isAccessibleDir) {
+    if (isAccessibleDir === undefined) {
       const filesInDir = await fs.readdir(userDir)
-      console.log(filesInDir[0])
-      const deletedFile = path.join(userDir, '/', filesInDir[0])
-      console.log('deleted', deletedFile)
-      // await fs.unlink(`${userDir}`)
-      await fs.unlink(deletedFile)
-
-      console.log('filesInDir', filesInDir)
-      // return
+      console.log(filesInDir)
+      // console.log(filesInDir[0])
+      if (filesInDir.length !== 0) {
+        // if (filesInDir.length === 0) return
+        const deletedFile = path.join(userDir, '/', filesInDir[0])
+        // console.log('deleted', deletedFile)
+        // await fs.unlink(`${userDir}`)
+        await fs.unlink(deletedFile)
+      }
     }
+
+    // console.log('filesInDir', filesInDir)
+    // return
+    // } else {
+    // if (isAccessibleDir === false) {
+    //   await fs.mkdir(userDir)
+    // }
+    // }
+    // if (isAccessibleDir === undefined) {
+    //   // await fs.unlink(userDir)
+    //   // await fs.rmdir(userDir)
+    //   const filesInDir = await fs.readdir(userDir)
+    //   // console.log(filesInDir[0])
+    //   if (filesInDir.length === 0) return
+    //   const deletedFile = path.join(userDir, '/', filesInDir[0])
+    //   // console.log('deleted', deletedFile)
+    //   // await fs.unlink(`${userDir}`)
+    //   await fs.unlink(deletedFile)
+
+  //   // console.log('filesInDir', filesInDir)
+  //   // return
+  //   // } else {
+  //   if (isAccessibleDir === false) {
+  //     await fs.mkdir(userDir)
+  //   }
+  // }
   } catch (error) {
     await fs.mkdir(userDir)
+    // throw error
   }
   // const isAccessibleDir = await fs.access(userDir)
   // console.log('accessible', isAccessibleDir)
@@ -60,13 +99,42 @@ const avatar = async (user, file) => {
   // await fs.mkdir(userDir)
   // const fileName = path.join(avatarDir, String(_id), originalname)
   const fileName = path.join(userDir, originalname)
+  // const updatedFile = await Jimp.read(tempPath)
+  // const updatedFile = await Jimp.read(newTempPath)
+  // await updatedFile.resize(250, 250).write(tempPath)
+  // await updatedFile.resize(250, 250).write(newTempPath)
+  // await Jimp.read(tempPath).resize(250, 250).write(tempPath)
   // console.log(fileName)
-  // try {
-  await fs.rename(tempPath, fileName)
-  // } catch (err) {
-  //   await fs.unlink(tempPath)
-  //   console.log(err)
-  // }
+  // console.log('updatedFile', updatedFile)
+  // console.log('tempPath after', newTempPath)
+  try {
+    const updatedFile = await Jimp.read(tempPath)
+    // const updatedFile = await Jimp.read(newTempPath)
+    // console.log(newTempPath);
+    // console.log(updatedFile)
+    // console.log(1)
+    await updatedFile.resize(250, 250).write(tempPath)
+    // const a = await updatedFile.resize(250, 250).write(tempPath)
+    // await updatedFile.resize(250, 250).write(newTempPath)
+    // console.log(a)
+    // console.log(newTempPath)
+    // console.log(2)
+    await fs.rename(tempPath, fileName)
+    // await fs.rename(, fileName)
+    // await fs.rename(newTempPath, fileName)
+    // console.log(3)
+    const avatarURL = `/avatars/${_id}/${originalname}`
+    const updatedUser = await User.findByIdAndUpdate(_id, { $set: { avatarURL: avatarURL } }, { new: true, runValidators: true })
+    return updatedUser
+    // await fs.rename(newTempPath, fileName)
+    // await fs.rename(updatedFile, fileName)
+    // await fs.rename(String(updatedFile), fileName)
+  } catch (err) {
+    await fs.unlink(tempPath)
+    // await fs.unlink(newTempPath)
+    // console.log(err)
+    throw err
+  }
 
   // получаем ссылку на папку с аватарками, которые будем раздавать из статики, не из базы
   // const avatarDir = path.join(__dirname, '../../', '/public/avatars')
@@ -106,10 +174,10 @@ const avatar = async (user, file) => {
   // await fs.rename(file.path, uploadDir)
   // const avatar = uploadDir
   // const avatarURL = `/public/avatars/${_id}/${originalname}`
-  const avatarURL = `/avatars/${_id}/${originalname}`
-  // const avatarURL = `/public/avatars/${user._id}/${file.originalname}`
-  // const updatedUser = await User.findOneAndUpdate(_id, { avatarURL: avatarURL })
-  const updatedUser = await User.findByIdAndUpdate(_id, { $set: { avatarURL: avatarURL } }, { new: true, runValidators: true })
+  // const avatarURL = `/avatars/${_id}/${originalname}`
+  // // const avatarURL = `/public/avatars/${user._id}/${file.originalname}`
+  // // const updatedUser = await User.findOneAndUpdate(_id, { avatarURL: avatarURL })
+  // const updatedUser = await User.findByIdAndUpdate(_id, { $set: { avatarURL: avatarURL } }, { new: true, runValidators: true })
   // const updatedUser = await User.findByIdAndUpdate(user._id, { $set: { avatarURL: avatarURL } }, { new: true, runValidators: true })
   // await User.findOneAndUpdate(user._id, { avatarURL })
   // if (!avatarURL) {
@@ -123,7 +191,7 @@ const avatar = async (user, file) => {
   // const updateAvatar
 
   // const updatedUser = await User.findByIdAndUpdate(user._id, { $set: { subscription: body.subscription } }, { new: true, runValidators: true })
-  return updatedUser
+  // return updatedUser
 }
 // avatar()
 // const avatar = async (user, file) => {
