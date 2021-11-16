799eb47bbabe36cb17057a7695daacda7ffc9a2b
JuliaZaykaS <sys55@mail.ru>
2021-11-16 10:32:22 +0300

    nodemailer & verification

--- model/users/registration.js
+++ model/users/registration.js
@@ -2,11 +2,26 @@ const bcrypt = require('bcrypt')
 require('dotenv').config()
 const gravatar = require('gravatar')
 const { v4: uuidv4 } = require('uuid')
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
 const { User } = require('../../db')
 const { AuthenticationError } = require('../../helpers')
 
-sgMail.setApiKey(process.env.SENDGRID_KEY)
+// sgMail.setApiKey(process.env.SENDGRID_KEY)
+const nodemailer = require('nodemailer')
+
+const { EMAIL_PASSWORD } = process.env
+
+const nodemailerConfig = {
+  host: 'smtp.mail.ru',
+  port: 465, // 25, 465, 2255
+  secure: true,
+  auth: {
+    user: 'marsi-anka55@mail.ru',
+    pass: EMAIL_PASSWORD
+  }
+}
+
+const transporter = nodemailer.createTransport(nodemailerConfig)
 
 const registration = async (body) => {
   const {
@@ -27,14 +42,26 @@ const registration = async (body) => {
 
   const user = new User({ email, password: hashedPassword, avatarURL, verificationToken })
   await user.save()
-  const msg = {
-    to: email, // Change to your recipient
-    from: 'marsi-anka55@mail.ru', // Change to your verified sender
-    subject: 'Thank you for registration!',
+
+  const emailForSend = {
+    to: email,
+    from: 'marsi-anka55@mail.ru',
+    subject: 'Регистрация на сайте',
     text: `Please, confirm your email address POST http://localhost:5555/users//verify/${verificationToken}`,
-    html: `Please, <a href="http://localhost:5555/users//verify/${verificationToken}">confirm</a> your email address`,
+    html: `Please, <a href="http://localhost:5555/users//verify/${verificationToken}">confirm</a> your email address`
   }
-  await sgMail.send(msg)
+
+  await transporter.sendMail(emailForSend)
+  // .then(() => console.log('Email success send'))
+  // .catch(error => console.log(error.message))
+  // const msg = {
+  //   to: email, // Change to your recipient
+  //   from: 'marsi-anka55@mail.ru', // Change to your verified sender
+  //   subject: 'Thank you for registration!',
+  //   text: `Please, confirm your email address POST http://localhost:5555/users//verify/${verificationToken}`,
+  //   html: `Please, <a href="http://localhost:5555/users//verify/${verificationToken}">confirm</a> your email address`,
+  // }
+  // await sgMail.send(msg)
 
   return user
 }
--- model/users/verification.js
+++ model/users/verification.js
@@ -10,12 +10,16 @@ const verification = async (verificationToken) => {
   }
   //   user.verificationToken = null
   //   user.verificationToken = ''
-  user.verify = true
-//   user.verificationToken = null
-//   user.verificationToken = 'null'
-  user.verificationToken = ' '
+  // user.verify = true
+  // user.verificationToken = null
+  // user.verificationToken = 'null'
+  // user.verificationToken = ' '
   //   const upd = await User.findByIdAndUpdate(user._id, { $set: { verificationToken: null, verify: true } }, { new: true, runValidators: true })
-  await user.save()
+  await User.findByIdAndUpdate(user._id, { verificationToken: null, verify: true })
+  // await user.save()
+  // user.verificationToken = null
+  // await user.save()
+
   //   console.log('after', upd)
 //   return user
 //   return upd
