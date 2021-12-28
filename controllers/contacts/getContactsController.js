8afe50b478cfd22d5a0c07a902fd6ab1f39938b2
JuliaZaykaS <sys55@mail.ru>
2021-12-28 16:23:07 +0300

    try to fix get all by swagger

--- controllers/contacts/getContactsController.js
+++ controllers/contacts/getContactsController.js
@@ -3,12 +3,63 @@ const { HTTPCodes } = require('../../helpers')
 
 const getContactsController = async (req, res, next) => {
   const { _id: owner } = req.user
-
-  let { page = 1, limit = 20, favorite } = req.query
-  console.log('favorite', favorite)
-
+  // let { page = 1, limit = 20, favorite } = req.query
+  let { page, limit, favorite } = req.query
+  console.log(req.query)
+  if (page === undefined || page === '') {
+    console.log('before', page)
+    page = 1
+    console.log('after',page)
+  }
+  if (limit === undefined || limit === '') {
+    limit = 20
+    // limit = 2
+  }
+  if (favorite === undefined || favorite === '') {
+    favorite = undefined
+  }
+  // limit = limit > 2 ? 2 : limit
   limit = limit > 20 ? 20 : limit
+  // const {page, limit, favorite} = req.query
+  // if (Object.keys(req.query).length === 0) {
+  // if (page !== '' || limit !== '' || favorite !== '') {
+  //   // let { page = 1, limit = 20, favorite } = req.query
+  //   console.log('req.query', req.query)
+  //   console.log('favorite', favorite)
+  //   console.log('page', page)
+  //   console.log('limit', limit)
 
+  //   limit = limit > 20 ? 20 : limit
+
+  //   // const contacts = await listContacts(owner, page, limit, favorite)
+  //   // // let { page = 1, limit = 20, favorite = undefined } = req.query
+
+  //   // res.status(HTTPCodes.OK).json(
+  //   //   { contacts: contacts, message: 'success', code: HTTPCodes.OK }
+  //   // )
+  // }
+  // // let { page = 1, limit = 20, favorite } = req.query
+  // // }
+  // if (page === '' || limit === '' || favorite === '') {
+  //   const queryObj = { ...req.query }
+  //   queryObj.page = 1
+  //   queryObj.limit = 20
+  //   queryObj.limit = queryObj.limit > 20 ? 20 : queryObj.limit
+  //   queryObj.favorite = undefined
+  //   // const contacts = await listContacts(owner, queryObj.page, queryObj.limit, queryObj.favorite)
+
+  //   // res.status(HTTPCodes.OK).json(
+  //   //   { contacts: contacts, message: 'success', code: HTTPCodes.OK }
+  //   //   )
+  // }
+
+  // const contacts = await listContacts(owner, page, limit, favorite)
+  // // let { page = 1, limit = 20, favorite = undefined } = req.query
+
+  // res.status(HTTPCodes.OK).json(
+  //   { contacts: contacts, message: 'success', code: HTTPCodes.OK }
+  // )
+  // limit = limit > 20 ? 20 : limit
   const contacts = await listContacts(owner, page, limit, favorite)
 
   res.status(HTTPCodes.OK).json(
--- model/contacts/listContacts.js
+++ model/contacts/listContacts.js
@@ -2,13 +2,18 @@ const { Contact } = require('../../db')
 
 const listContacts = async (owner, page, limit, favorite) => {
   console.log('fav', favorite)
+  console.log('page', page)
+  console.log('limit', limit)
   console.log('type', typeof favorite)
   if (favorite === undefined) {
+  // if (favorite === '') {
     const contacts = await Contact.paginate({ owner }, { page, limit })
+    console.log(contacts)
     // const contacts = await Contact.paginate({ owner }, { page, limit })
     return contacts
   }
   // const fav = Boolean(favorite)
+  // console.log('fav after', fav)
   // const contacts = await Contact.paginate({ owner, fav }, { page, limit })
   const contacts = await Contact.paginate({ owner, favorite }, { page, limit })
   // if (favorite === 'true') {
