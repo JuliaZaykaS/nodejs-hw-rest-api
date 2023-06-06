93a1453e61334d67f8733f2b4c2838d43ab3e591
Zayka Julia <zaykajulias@gmail.com>
2023-06-06 12:35:51 +0300

    fix verification settings

--- helpers/verificationMessage.js
+++ helpers/verificationMessage.js
@@ -1,28 +1,30 @@
-const nodemailer = require('nodemailer')
-require('dotenv').config()
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const verificationMessage = async(email, verificationToken) => {
-  const { EMAIL_PASSWORD } = process.env
+const verificationMessage = async (email, verificationToken) => {
+  const { EMAIL_PASSWORD, BACKEND_URL, SEND_EMAIL } = process.env;
 
   const nodemailerConfig = {
-    host: 'smtp.mail.ru',
+    host: "smtp.mail.ru",
     port: 465,
     secure: true,
     auth: {
-      user: 'marsi-anka55@mail.ru',
-      pass: EMAIL_PASSWORD
-    }
-  }
-  const transporter = nodemailer.createTransport(nodemailerConfig)
+      // user: "marsi-anka55@mail.ru",
+      user: SEND_EMAIL,
+      pass: EMAIL_PASSWORD,
+    },
+  };
+  const transporter = nodemailer.createTransport(nodemailerConfig);
   const emailForSend = {
     to: email,
-    from: 'marsi-anka55@mail.ru',
-    subject: 'Регистрация на сайте',
-    text: `Please, confirm your email address POST https://backend-for-phonebook.herokuapp.com/users//verify/${verificationToken}`,
-    html: `Please, <a href="https://backend-for-phonebook.herokuapp.com/users/verify/${verificationToken}">confirm</a> your email address`
-  }
+    // from: "marsi-anka55@mail.ru",
+    from: SEND_EMAIL,
+    subject: "Регистрация на сайте",
+    text: `Please, confirm your email address POST ${BACKEND_URL}/users/verify/${verificationToken}`,
+    html: `Please, <a href="${BACKEND_URL}/users/verify/${verificationToken}">confirm</a> your email address`,
+  };
 
-  transporter.sendMail(emailForSend)
-}
+  transporter.sendMail(emailForSend);
+};
 
-module.exports = verificationMessage
+module.exports = verificationMessage;
